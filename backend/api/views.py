28b18de544ef1944972cb4c5eb4a3c991a6983d2
Rezne Josef <solinvictus3956@gmail.com>
2023-02-23 11:51:33 +0800

    Initial Commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,14 @@
-import json
-from django.http import JsonResponse, HttpResponse
+
 from product.models import Product
 from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from product.serializers import ProductSerializer
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
-
-
-    if model_data:
-       data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    instance = Product.objects.all().order_by("?").first()
+    data = {}
+    if instance:
+        data = ProductSerializer(instance).data
+       # data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
     return Response(data)
