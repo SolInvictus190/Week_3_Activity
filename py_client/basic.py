b12ad5303c5cce9fd4a3a82fc7e2c5f732f7d475
Rezne Josef <solinvictus3956@gmail.com>
2023-02-23 09:40:40 +0800

    Initial Commit

--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,7 @@ import requests
 #endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={"product_id": 123})
+get_response = requests.get(endpoint, json={"product_id": 123})
 # print(get_response.text)
 # print(get_response.status_code)
 
