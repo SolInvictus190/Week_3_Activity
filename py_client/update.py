141c2384a55b622e4fdceee30cfc7ce544e2e130
Rezne Josef <solinvictus3956@gmail.com>
2023-02-27 11:31:11 +0800

    Initial Commit

---
+++ py_client/update.py
@@ -0,0 +1,16 @@
+import requests
+
+
+endpoint = "http://localhost:8000/api/product/1/update/"
+
+data = {"title": "Hello My Old Friend!",
+       "price": 140.99
+}
+
+get_response = requests.put(endpoint, json=data)
+
+print(get_response.json())
+
+
+
+
